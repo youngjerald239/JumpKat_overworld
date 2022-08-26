6e19b92be4513f48e45f4899398838caa914cba6
youngjerald239 <youngjerald239@gmail.com>
2022-08-25 20:01:02 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# JumpKat_overworld
---
+++ game_data.py
@@ -0,0 +1,6 @@
+level_0 = {'node_pos':(110,400), 'content': 'this is level 0', 'unlock':1}
+level_1 = {'node_pos':(110,400), 'content': 'this is level 0', 'unlock':2}
+level_2 = {'node_pos':(110,400), 'content': 'this is level 0', 'unlock':3}
+level_3 = {'node_pos':(110,400), 'content': 'this is level 0', 'unlock':4}
+level_4 = {'node_pos':(110,400), 'content': 'this is level 0', 'unlock':5}
+level_5 = {'node_pos':(110,400), 'content': 'this is level 0', 'unlock':5}
